6ca4ea7300bb34371199c9c8067abb526c48a997
isabellow <ilow1991@gmail.com>
2019-04-09 14:49:33 -0700

    updated to change file path and email

--- slrum_scripts/convertNL2bin.sh
+++ slrum_scripts/convertNL2bin.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH -p owners
+#SBATCH -p giocomo, owners, normal
 #SBATCH --job-name=nl2bin
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=attialex@stanford.edu
+#SBATCH --mail-user=ilow@stanford.edu
 #SBATCH --mem-per-cpu=50G
 #SBATCH --time=2:00:00
-#SBATCH -o /scratch/users/attialex/nl2bin.%N.%j.out # STDOUT
-#SBATCH -e /scratch/users/attialex/nl2bin.%N.%j.err # STDERR
+#SBATCH -o /scratch/users/ilow/nl2bin.%N.%j.out # STDOUT
+#SBATCH -e /scratch/users/ilow/nl2bin.%N.%j.err # STDERR
 
 module load matlab
 echo "$(date): job $SLURM_JOBID starting on $SLURM_NODELIST"
 
-matlab -nodisplay -nosplash -r "run $HOME/AlexA_Library/NeuroPixel/convertNL2binScript.m, exit"
+matlab -nodisplay -nosplash -r "run $HOME/SiliconProbeCode/convertNL2binScript.m, exit"
