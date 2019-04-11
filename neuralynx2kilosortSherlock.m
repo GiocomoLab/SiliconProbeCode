386f8be16c5157985536e69051a075ca8a33d240
Isabel Low <ilow1991@gmail.com>
2019-04-10 18:23:55 -0700

    made script to concat several sessions and updated NL2bin to reflect

--- neuralynx2kilosortSherlock.m
+++ neuralynx2kilosortSherlock.m
@@ -1,13 +1,13 @@
 % directoryPath = 'F:\H1_flipped\2019-03-12_10-21-28'
-function neuralynx2kilosortSherlock(directories, target_dir)
+function filename = neuralynx2kilosortSherlock(directoryPath, target_dir)
     % convert H3 probe data to bin format for kilosort analysis
     % created by Kei Masuda
     % IL edited 4/9/19
     %
     % Inputs:
     % -------
-    % directories : cell array
-    %   contains path(s) to data folders, no trailing backslash
+    % directory : string
+    %   contains path to data folder, no trailing backslash
     % target_dir : string
     %   path to folder to save output; optional, default is directorypath
     %
@@ -18,51 +18,43 @@ function neuralynx2kilosortSherlock(directories, target_dir)
     %   order of recording
     
     % check input arguments
-    if iscell(directories)
-        directories = string(directories);
+    if iscell(directoryPath)
+        directoryPath = string(directoryPath);
     end
     if nargin == 1
-        target_dir = directories{1};
+        target_dir = directoryPath;
     end
     
     fprintf(strcat('\nStart Processing: ', datestr(now,'mmmm dd, yyyy HH:MM:SS AM'),'\n'));
     numOfChannels = 64;
-    num_files = size(directories, 2);
-    sample_idx = [1 zeros(1, num_files)];
     
-    for dir = 1:num_files
-        directoryPath = directories{dir};
-        fprintf(strcat('\nProcessing session ', num2str(dir), ' out of ', num2str(num_files), '\n'))
-        for csc = 1:numOfChannels
-            cscPath = fullfile(directoryPath, ['CSC_HP_' num2str(csc) '.ncs']);
-            %cscPath = fullfile(directoryPath, ['CSC' num2str(csc) '.ncs']);
+    for csc = 1:numOfChannels
+        cscPath = fullfile(directoryPath, ['CSC_HP_' num2str(csc) '.ncs']);
+        %cscPath = fullfile(directoryPath, ['CSC' num2str(csc) '.ncs']);
 
-            % load neuralynx file, linearize samples, convert to int16
-            [Samples,header]=Nlx2MatCSC_v3(cscPath, [0 0 0 0 1], 1, 1, [] );
-            tmp=split(header{17}); %assuming conversion factor is in here;
-            conv_factor = str2double(tmp{2}); % in volts
-            conv_factor = conv_factor*10e6; % in micro volts
-            %conv_factor = 1;
-            Samples = reshape(Samples,1,[])*-1*conv_factor; 
-            Samples= int16(Samples); 
+        % load neuralynx file, linearize samples, convert to int16
+        [Samples,header]=Nlx2MatCSC_v3(cscPath, [0 0 0 0 1], 1, 1, [] );
+        tmp=split(header{17}); %assuming conversion factor is in here;
+        conv_factor = str2double(tmp{2}); % in volts
+        conv_factor = conv_factor*10e6; % in micro volts
+        %conv_factor = 1;
+        Samples = reshape(Samples,1,[])*-1*conv_factor; 
+        Samples= int16(Samples); 
 
-            % pre-allocate dataMatrix size on first pass of each session
-            if dir == 1 && csc == 1 
-               sample_idx(dir + 1) = size(Samples,2);
-               dataMatrix = zeros(numOfChannels,sample_idx(dir + 1),'int16');
-            elseif csc == 1
-               sample_idx(dir + 1) = size(Samples,2);
-               dataMatrix = [dataMatrix zeros(numOfChannels,sample_idx(dir + 1),'int16')];
-            end
-            
-            % allocate csc data to appropriate rows/columns of data matrix
-            dataMatrix(csc, sample_idx(dir):sample_idx(dir + 1)) = Samples; 
-            fprintf(strcat('\nProcessed: ', num2str(csc), ' out of 64 CSC files.'));
-        end    
-    end
+        % pre-allocate dataMatrix size on first pass of each session
+        if csc == 1 
+           sample_idx = size(Samples,2);
+           dataMatrix = zeros(numOfChannels,sample_idx,'int16');
+        end
+
+        % allocate csc data to appropriate rows/columns of data matrix
+        dataMatrix(csc, sample_idx(dir):sample_idx(dir + 1)) = Samples; 
+        fprintf(strcat('\nProcessed: ', num2str(csc), ' out of 64 CSC files.'));
+    end    
     
     [~,Name,~] = fileparts(directoryPath);
-    fid = fopen(fullfile(target_dir,[Name '_dataMatrix.bin']), 'w'); 
+    filename = [Name '_dataMatrix.bin'];
+    fid = fopen(fullfile(target_dir, filename), 'w'); 
     fwrite(fid, dataMatrix, 'int16');
     fclose(fid);
     fprintf(strcat('\nDone Processing: ', datestr(now,'mmmm dd, yyyy HH:MM:SS AM'),'\n'));
